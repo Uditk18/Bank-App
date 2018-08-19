550e73ed4e443219f9b8c4553fd245cf98329365
Uditk18 <karwasraudit@gmail.com>
2018-08-20 00:20:52 +0530

    Update BankServlet.java

--- src/com/app/bank/AppController/BankServlet.java
+++ src/com/app/bank/AppController/BankServlet.java
@@ -82,10 +82,42 @@ public class BankServlet extends HttpServlet {
 			dispatcher = request.getRequestDispatcher(redirect);
 			dispatcher.forward(request, response);
 			break;
+				
+		case "/fundTransfer1.app":
+			bank2=service.showSavings();
+			int flag2=0;
+			String page1="";
+			int senderAccountNumber = Integer.parseInt(request.getParameter("senderAccountNumber"));
+			int receiverAccountNumber = Integer.parseInt(request.getParameter("receiverAccountNumber"));
+			double transferAmount=Double.parseDouble(request.getParameter("transferAmount"));
+			
+			for(BankAccount account3 : bank2) {
+				if(senderAccountNumber==account3.getAccountNumber()) {
+					flag2=1;
+					for(BankAccount account4 : bank2) {
+						if(receiverAccountNumber==account4.getAccountNumber()) {
+							debitedBalance=service.withdraw(transferAmount, senderAccountNumber);
+							creditedBalance=service.deposit(transferAmount, receiverAccountNumber);
+							request.setAttribute("debit", debitedBalance);
+							request.setAttribute("credit", creditedBalance);
+							page1="SuccessTransfer.jsp";
+							flag2=2;
+						}
+					}
+				}
+			}
+			    if(flag2==0) {
+			    	page1="sendererror.jsp";
+			    }
+			    if(flag2==1) {
+			    	page1="receivererror.jsp";
+			    }
+				  RequestDispatcher dispatcher1 = request.getRequestDispatcher(page1);
+				  dispatcher1.forward(request, response);	
+			break;
 		/*case "/deposit2.app":
 			break;
-		case "/fundTransfer1.app":
-			break;
+		
 		case "/fundTransfer2.app":
 			break;*/
 			
